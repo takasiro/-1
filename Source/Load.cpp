442bcdff410bc0f7466ff7772522ec64d461ab04
CSato2000 <bambeaman334@gmail.com>
2019-08-05 15:33:00 +0900

    [update]Load進捗
    ファイル読み込み処理の内容の効率が上がったかもしれないです

--- Source/Load.cpp
+++ Source/Load.cpp
@@ -1,23 +1,55 @@
 #include<DxLib.h>
 #include"Load.h"
-/*******
+void Load::LoadData(int _fileHandle,char* _data) {
+	for (int i = 0; i < 64; i++) {
+		_data[i] = FileRead_getc(_fileHandle);  //ファイル読み込み
+		if (_data[i] == ',' || _data[i] == '\n') {//カンマか改行の場合
+			_data[i] = '\0';  //そこまでを文字列とする
+			break;
+		}
+	}
+}
+
+/******************************************************
 型:void
 引数:const char* file_Path…読み込むファイルのパス
 　　 vector<Mass>& _mass…マップの地形情報を格納する　
-*****/
-void Load::LoadMap(char* _filePath, vector<Mass>& _mass) {
+*******************************************************/
+int Load::LoadMap(const char* _filePath, vector<Mass>& _mass) {
 	//二次元配列の代入　引数の型　
 	int stageHandle=FileRead_open(_filePath); 
-	char input[256];  //ファイル読み込み用(char)
+	char *inputc;  //ファイル読み込み用(char)
 
-	while (FileRead_eof(stageHandle) == 0) {  //ファイルの終端になるまで
+	if (stageHandle = NULL)return -1;
+
+	while (FileRead_eof(stageHandle) == 0) {  //ファイルの終端まで
+		LoadData(stageHandle, inputc);  //データ読み込み			 
+		_mass.emplace_back(Mass(0, atoi(inputc), 0));  //Mass配列に代入
+	}
+
+	return 0;
+}	
+
+void Load::PlayerBase(char* _filePath, vector<Unit>& _unit) {
+	int stageHandle = FileRead_open(_filePath);
+	int n=0,num=0;
+	//最初の二行　読み飛ばす　処理　
+	for (int i = 0; i < 2; i++)while (FileRead_getc(stageHandle) != '\n');
+	char input[64];
+	//ファイルの読み取り処理(breakでwhile文を抜ける処理なので条件式は1に設定されている)
+	while (FileRead_eof(stageHandle) == 0) {
+
 		for (int i = 0; i < 64; i++) {
-			input[i] = FileRead_getc(stageHandle);
-			if (input[i] == ',' || input[i] == '\n') {//カンマか改行の場合
-				input[i] = '\0';  //そこまでを文字列とする
+			input[i] = FileRead_getc(stageHandle);//1文字取得する
+			if (input[i] == '/') {//スラッシュがあれば
+				while (FileRead_getc(stageHandle) != '\n');//改行までループ
+				i = -1;//カウンタを最初に戻して
+				continue;
+			}
+			if (input[i] == ',' || input[i] == '\n') {//カンマか改行なら
+				input[i] = '\0';//そこまでを文字列とし
 				break;
 			}
 		}
-		_mass.emplace_back(Mass(0,atoi(input),0));  //Mass配列に代入
 	}
-}		
+}
--- Source/Load.h
+++ Source/Load.h
@@ -3,6 +3,7 @@
 using namespace std;
 #include<string>
 #include"Map.h"
+#include"Unit.h"	
 #include"BaseTask.h"
 
 
@@ -10,10 +11,19 @@ class Load:public BaseTask {
 private:
 
 public:
-	Load();
-	virtual ~Load();
+	Load() {};
+	virtual ~Load(){};
 
-	void LoadMap(char*, vector<Mass>&);  //マップロード関数 
+	//void LoadData(const char*,char*);
+	void LoadData(int,char*);
+
+	int LoadMap(const char*, vector<Mass>&);  //マップロード関数 
+	
+	void PlayerBase(char*, vector<Unit>&);  //キャラクターステータスのロード関数 
+	//void PlayerGrowth(char*, vector<Unit>&);  //キャラクター成長値のロード
+	
+	void FairyBase();
+	void FairyGrowth();
 
 	int Initialize() { return 0; }	//初期化処理
 	int Update() { return 0; }		//計算処理

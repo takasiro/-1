538ae8981062b1c8dd1d66e73022b29f5e4b462a
takasiro <takagiyda@gmail.com>
2019-08-05 09:14:23 +0900

    [update]エネミークラスの作成
    エネミークラスを追加しました

--- Source/Enemy.h
+++ Source/Enemy.h
@@ -9,13 +9,13 @@ protected:
 
 public:
 	Enemy();
-	Enemy(eRole _role, string* _name, int _hp, int _str, int _def, int _mnd, int _mdef,
+	Enemy(eRole _role, string _name, int _hp, int _str, int _def, int _mnd, int _mdef,
 		int _dex, int _agi, int _move, int _exp, int _lv);
 	virtual ~Enemy();
 
 	Unit* GetEnemy() { GetUnit(); }
 	int Initialize();//初期化処理
-	int Initialize(eRole _role, string* _name, int _hp, int _str, int _def, int _mnd, int _mdef,
+	int Initialize(eRole _role, string _name, int _hp, int _str, int _def, int _mnd, int _mdef,
 		int _dex, int _agi, int _move, int _exp, int _lv);	//初期化処理(引数付き)
 	int Update();	//計算処理
 	int Draw();		//描画処理
@@ -24,7 +24,7 @@ public:
 
 
 	int LastStatus();//武器補正含めた最終ステータス
-
+	//バグるのでコメントアウト
 	int SetFairy() {
 		//バグるのでコメントアウト
 		//mEquipSlot = new Fairy();

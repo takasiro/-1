d68ec33f61cf94beff636b93a03bf908b93f2ba6
CSato2000 <bambeaman334@gmail.com>
2019-08-30 13:43:22 +0900

    [update]プレイヤー成長値の追加

--- Source/Load.cpp
+++ Source/Load.cpp
@@ -45,55 +45,86 @@ int Load::LoadData(const char* _filePath, vector<Mass>& _mass) {
 }
 
 
-int Load::LoadData(char* _filePath, vector<Unit>& _unit) {
+int Load::LoadData(const char* _baseFilePath, const char* _growthFilePath, vector<Unit>& _unit) {
 	//ファイルオープン
-	int stageHandle = FileRead_open(_filePath);
+	baseHandle = FileRead_open(_baseFilePath);
+	growthHandle = FileRead_open(_growthFilePath);
 
 	char input[256];
 
-	char name[256];	//名前
-	eRole	role;	//そのキャラの職業(成長値に偏りを出させるため)
-	int hp = 0;		//体力
-	int str = 0;		//力(物理用)
-	int def = 0;		//防御(物理用)
-	int intelli = 0;		//魔力(魔法用)
-	int mnd = 0;		//魔防(魔法用)
-	int dex = 0;		//	命中
-	int agi = 0;		//回避
-	int move = 0;	//移動力
+	hp = 0;		//体力
+	str = 0;		//力(物理用)
+	def = 0;		//防御(物理用)
+	intelli = 0;		//魔力(魔法用)
+	mnd = 0;		//魔防(魔法用)
+	dex = 0;		//	命中
+	agi = 0;		//回避
+	move = 0;	//移動力
+
+	gHp = 0;
+	gStr = 0;
+	gDef = 0;
+	gIntelli = 0;
+	gMnd = 0;
+	gDex = 0;
+	gAgi = 0;
 
 	int n = 0;
+	int nn = 0;
 
 
-	if (stageHandle == NULL)return -1;
+	if (baseHandle == NULL)return -1;
+	if (growthHandle == NULL)return -1;
+	while (FileRead_eof(baseHandle) == 0) {
+		while (FileRead_eof(baseHandle) == 0) {  //ファイルの終端まで
+			LoadFile(baseHandle, input);  //データ読み込み	
 
-	while (FileRead_eof(stageHandle) == 0) {  //ファイルの終端まで
-		LoadFile(stageHandle, input);  //データ読み込み	
+			switch (n) {
+			case 0:strcpy(name, input); break;
+			case 1:role = eRole(atoi(input)); break;
+			case 2:hp = atoi(input); break;
+			case 3:str = atoi(input); break;
+			case 4:def = atoi(input); break;
+			case 5:intelli = atoi(input); break;
+			case 6:mnd = atoi(input); break;
+			case 7:dex = atoi(input); break;
+			case 8:agi = atoi(input); break;
+			case 9:move = atoi(input); break;
+			}
 
-		switch (n) {
-		case 0:strcpy(name, input); break;
-		case 1:role = eRole(atoi(input)); break;
-		case 2:hp = atoi(input); break;
-		case 3:str = atoi(input); break;
-		case 4:def = atoi(input); break;
-		case 5:intelli = atoi(input); break;
-		case 6:mnd = atoi(input); break;
-		case 7:dex = atoi(input); break;
-		case 8:agi = atoi(input); break;
-		case 9:move = atoi(input); break;
+			n++;
+
+			if (n == 10) {
+				_unit.emplace_back(Unit(name, role, hp, str, def, intelli, mnd, dex, agi, move, 0, 0));
+
+				n = 0;
+				break;
+			}
+
 		}
+		while (FileRead_eof(growthHandle) == 0) {  //ファイルの終端まで
+			LoadFile(growthHandle, input);  //データ読み込み	
 
-		n++;
+			switch (nn) {
+			case 0:gHp = atof(input); break;
+			case 1:gStr = atof(input); break;
+			case 2:gDef = atof(input); break;
+			case 3:gIntelli = atof(input); break;
+			case 4:gMnd = atof(input); break;
+			case 5:gDex = atof(input); break;
+			case 6:gAgi = atof(input); break;
+			}
+			nn++;
 
-		if (n == 10) {
-			_unit.emplace_back(Unit(name, role, hp, str, def, intelli, mnd, dex, agi, move, 0, 0));
-			n = 0;
+			if (nn == 7) {
+				_unit[_unit.size() - 1].SetGrowth(gHp, gStr, gDef, gIntelli, gMnd, gDex, gAgi);
+				nn = 0;
+				break;
+			}
 		}
-
 	}
 
-	FileRead_close(stageHandle);
-
+	FileRead_close(baseHandle);
+	FileRead_close(growthHandle);
 	return 0;
-
 }
--- Source/Load.h
+++ Source/Load.h
@@ -11,6 +11,29 @@ using namespace std;
 
 class Load :public BaseTask {
 private:
+	int baseHandle;
+	int growthHandle;
+
+	char input[256];
+
+	char name[256];	//名前
+	eRole	role;	//そのキャラの職業(成長値に偏りを出させるため)
+	int hp;		//体力
+	int str;		//力(物理用)
+	int def;		//防御(物理用)
+	int intelli;		//魔力(魔法用)
+	int mnd;		//魔防(魔法用)
+	int dex;		//	命中
+	int agi;		//回避
+	int move;	//移動力
+
+	double gHp;
+	double gStr;
+	double gDef;
+	double gIntelli;
+	double gMnd;
+	double gDex;
+	double gAgi;
 
 public:
 	Load() {};
@@ -20,11 +43,7 @@ public:
 
 	int LoadData(const char*, vector<Mass>&);  //マップロード関数 
 
-	int LoadData(char*, vector<Unit>&);  //キャラクターステータスのロード関数 
-	//void PlayerGrowth(char*, vector<Unit>&);  //キャラクター成長値のロード
-
-	void FairyBase();
-	void FairyGrowth();
+	int LoadData(const char* _baseFilePath, const char* _growthFilePath, vector<Unit>&);  //キャラクターステータスのロード関数 
 
 	int Initialize() { return 0; }	//初期化処理
 	int Update() { return 0; }		//計算処理

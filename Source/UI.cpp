26441895dddae4122cf4614313e8a013166d842d
CSato2000 <bambeaman334@gmail.com>
2019-10-26 02:06:55 +0900

    [update]必要ステータスがすべて表示されるようにしました

--- Source/UI.cpp
+++ Source/UI.cpp
@@ -1,28 +1,60 @@
 #include"UI.h"
+int UI::Initialize() {
+	playerNumber = 0, enemyNumber = 0;
+	role = "ノージョブ";
+	NameFontHandle = CreateFontToHandle(NULL, 35, 4);
+	DataFontHandle = CreateFontToHandle(NULL, 25, 2);
+	StatusFontHandle = CreateFontToHandle(NULL, 20, 4);
+	return 0;
+}
 
 int UI::Update() {
 	BaseObj::sPos  tmp;
-	num = INSTANCE->CulNum(tmp = GET_POSITION(), PLAYER);
+	playerNumber = INSTANCE->CulNum(tmp = GET_POSITION(), PLAYER);
 
 	return 0;
 }
 
 int UI::Draw() {
-	if (num != -1)DrawStatus();
-	DrawFormatString(0, 120, GetColor(255, 255, 255), "num:%d", num, TRUE);
+	if (playerNumber != -1)DrawStatus(playerNumber);
+
+	#ifdef DEBUG
+	DrawFormatString(0, 300, GetColor(255, 255, 255), "num:%d", playerNumber, TRUE);
+	#endif
 
 	return 0;
 }
 
-int UI::DrawStatus() {
+int UI::DrawStatus(int _num) {
 	vector<Chara*>& charaStatus = INSTANCE->GetCharaDate();
-	DrawBox(0, 0, 500, 300, GetColor(180, 140, 220), true);
-	DrawFormatString(0, 140, GetColor(255, 255, 255), "ファイタロウLv:%d", charaStatus[num]->GetLv(), TRUE);
-	DrawFormatString(0, 180, GetColor(255, 255, 255), "ファイタロウStr:%d", charaStatus[num]->GetStr(), TRUE);
-	DrawFormatString(0, 200, GetColor(255, 255, 255), "ファイタロウDef:%d", charaStatus[num]->GetDef(), TRUE);
-	DrawFormatString(0, 220, GetColor(255, 255, 255), "ファイタロウInt:%d", charaStatus[num]->GetInt(), TRUE);
-	DrawFormatString(0, 240, GetColor(255, 255, 255), "ファイタロウMnd:%d", charaStatus[num]->GetMnd(), TRUE);
-	DrawFormatString(0, 260, GetColor(255, 255, 255), "ファイタロウDex:%d", charaStatus[num]->GetDex(), TRUE);
+	string tmpName = charaStatus[_num]->GetName();
+	int tmpRole = charaStatus[_num]->GetRole();
+	role=RoleType(tmpRole);
+
+
+	//ステータス表示
+	DrawBox(0, 0, 400, 250, GetColor(180, 140, 220), true);
+	DrawFormatStringToHandle(10, 20, GetColor(255, 255, 255),NameFontHandle, "%s", tmpName.c_str());
+	DrawFormatStringToHandle(10, 60, GetColor(255, 255, 255),DataFontHandle, "%s",role.c_str(),TRUE);
+	DrawFormatStringToHandle(15, 90, GetColor(255, 255, 255),StatusFontHandle, "EXP:%d",00, TRUE);
+	DrawFormatStringToHandle(15, 120, GetColor(255, 255, 255),DataFontHandle, "Lv.%d", charaStatus[_num]->GetLv(), TRUE);
+	DrawFormatStringToHandle(100, 120, GetColor(255, 255, 255), DataFontHandle,"HP:%d/%d", charaStatus[_num]->GetStr(), charaStatus[_num]->GetStr(), TRUE);
+	DrawFormatStringToHandle(15, 150, GetColor(255, 255, 255), StatusFontHandle,"攻撃:%d", charaStatus[_num]->GetStr(), TRUE);
+	DrawFormatStringToHandle(120, 150, GetColor(255, 255, 255), StatusFontHandle, "防御:%d", charaStatus[_num]->GetDef(), TRUE);
+	DrawFormatStringToHandle(15,  180, GetColor(255, 255, 255), StatusFontHandle, "魔法:%d", charaStatus[_num]->GetInt(), TRUE);
+	DrawFormatStringToHandle(120, 180, GetColor(255, 255, 255), StatusFontHandle, "魔防:%d", charaStatus[_num]->GetMnd(), TRUE);
+	DrawFormatStringToHandle(15,  210, GetColor(255, 255, 255), StatusFontHandle, "器用さ:%d", charaStatus[_num]->GetDex(), TRUE);
+	DrawFormatStringToHandle(120, 210, GetColor(255, 255, 255), StatusFontHandle, "素早さ:%d", charaStatus[_num]->GetAgi(), TRUE);
 
 	return 0;
 }
+
+string UI::RoleType(int _type) {
+	switch (_type) {
+	case eTank: return roleType.tank; break;
+	case eCaster: return roleType.caster; break;
+	case eRanger: return roleType.ranger; break;
+	case eFighter: return roleType.fighter;  break;
+	}
+}
+

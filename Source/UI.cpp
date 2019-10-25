ec1029962397f4af71ae2399dd5133f678840c49
CSato2000 <bambeaman334@gmail.com>
2019-10-25 17:57:33 +0900

    [update]取得可能なプレイヤーステータスを表示できるようにしました

--- Source/UI.cpp
+++ Source/UI.cpp
@@ -17,7 +17,12 @@ int UI::Draw() {
 int UI::DrawStatus() {
 	vector<Chara*>& charaStatus = INSTANCE->GetCharaDate();
 	DrawBox(0, 0, 500, 300, GetColor(180, 140, 220), true);
-	DrawFormatString(0, 160, GetColor(255, 255, 255), "ふぁいたろう:%d", charaStatus[0]->GetLv(), TRUE);
+	DrawFormatString(0, 140, GetColor(255, 255, 255), "ファイタロウLv:%d", charaStatus[num]->GetLv(), TRUE);
+	DrawFormatString(0, 180, GetColor(255, 255, 255), "ファイタロウStr:%d", charaStatus[num]->GetStr(), TRUE);
+	DrawFormatString(0, 200, GetColor(255, 255, 255), "ファイタロウDef:%d", charaStatus[num]->GetDef(), TRUE);
+	DrawFormatString(0, 220, GetColor(255, 255, 255), "ファイタロウInt:%d", charaStatus[num]->GetInt(), TRUE);
+	DrawFormatString(0, 240, GetColor(255, 255, 255), "ファイタロウMnd:%d", charaStatus[num]->GetMnd(), TRUE);
+	DrawFormatString(0, 260, GetColor(255, 255, 255), "ファイタロウDex:%d", charaStatus[num]->GetDex(), TRUE);
 
 	return 0;
 }

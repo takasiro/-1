30f84e7305a79abab6af9ca67d525488f71f647a
takumi0818 <taihara18yda@gmail.com>
2019-11-08 15:15:29 +0900

    <update>マップセレクト画面の変更
    マップセレクト画面の変更

--- Source/MapSelectScene.cpp
+++ Source/MapSelectScene.cpp
@@ -2,13 +2,7 @@
 #include "MapSelectScene.h"
 
 MapSelectScene::MapSelectScene() {
-	for (int i = 0; i < 10; i++) {
-		map.push_back(Unit{ 100.0 * i + 20,980 / 2,20,i , });
-		if (i != 9)map[i].next.push_back(i + 1);
-		map[i].prev.push_back(NULL + i - 1);
-		//if (i == 4)map[i].next.push_back(10);
 
-	}
 }
 
 
@@ -24,51 +18,54 @@ int MapSelectScene::Initialize() {
 	return 0;
 }
 int MapSelectScene::Update() {
-	GetMousePoint(&MouseX, &MouseY);
 
-	for (int j = 0; j < map[player.pos].next.size(); j++) {
-		if (map[player.pos].next[j] != NULL) {
-			//プレイヤーが今いる場所からつながっている場所をクリックしたらという判定
-			if ((map[map[player.pos].next[j]].x - MouseX) * (map[map[player.pos].next[j]].x - MouseX) + (map[map[player.pos].next[j]].y - MouseY) *
-				(map[map[player.pos].next[j]].y - MouseY) < (map[map[player.pos].next[j]].r * map[map[player.pos].next[j]].r)) {
-				player.x = map[map[player.pos].next[j]].x;
-				player.y = map[map[player.pos].next[j]].y;
-				player.pos = map[player.pos].next[j];
+	//マウス座標取得
+	GetMousePoint(&MouseX, &MouseY);
+	for (int i = 0; i < 9; i++) {
+		for (int j = 0; j < 9; j++) {
+			if (map[i].connection[j] != -1) {
+				if ((map[i].x - MouseX) * (map[i].x - MouseX) + (map[i].y - MouseY) *
+					(map[i].y - MouseY) <= (map[i].r * map[i].r)) {
+					if (map[i].connection[0] == state) {
+						state = i;
+					}
+					if (map[i].connection[1] == state) {
+						state = i;
+					}
+				}
 			}
 		}
 	}
-	for (int j = 0; j < map[player.pos].prev.size(); j++) {
-		if (map[player.pos].prev[j] != NULL) {
-			if ((map[map[player.pos].prev[j]].x - MouseX) * (map[map[player.pos].prev[j]].x - MouseX) + (map[map[player.pos].prev[j]].y - MouseY) *
-				(map[map[player.pos].prev[j]].y - MouseY) <= (map[map[player.pos].prev[j]].r * map[map[player.pos].prev[j]].r)) {
-				player.x = map[map[player.pos].prev[j]].x;
-				player.y = map[map[player.pos].prev[j]].y;
-				player.pos = map[player.pos].prev[j];
-			}
-		}
-	}
+
 	return 0;
 }
 int MapSelectScene::Draw() {
-	GetMousePoint(&MouseX, &MouseY);
 
-	//以下表示
-	for (int i = 0; i < map.size(); i++) {
-		DrawCircle(map[i].x, map[i].y, map[i].r, GetColor(255, 255, 255));
-		for (int j = 0; j < map[i].next.size(); j++) {
-			DrawLine(map[i].x, map[i].y, map[map[i].next[j]].x, map[map[i].next[j]].y, GetColor(255, 255, 255));
+	for (int i = 0; i < 9; i++) {
+		for (int j = 0; j < 2; j++) {
+			if (map[i].connection[j] != -1) {
+				if (map[i].connection[j] == state) {
+					DrawCircle(map[i].x, map[i].y, 30, GetColor(100, 100, 255), true);
+					for (int k = 0; k < 4; k++) {
+						DrawLine(
+							map[i].x + direction[k][0],
+							map[i].y + direction[k][1],
+							map[map[i].connection[j]].x + direction[k][0],
+							map[map[i].connection[j]].y + direction[k][1],
+							GetColor(100, 255, 100));
+					}
+				}
+				DrawLine(map[i].x, map[i].y, map[map[i].connection[j]].x, map[map[i].connection[j]].y, GetColor(0, 100, 0));
+			}
 		}
-
 	}
-	for (int j = 0; j < map[player.pos].next.size(); j++) {
-		DrawFormatString(0 + 80 * j, 30, GetColor(255, 255, 255), "ネクスト%d", map[player.pos].next[j]);
+	for (int i = 0; i < 9; i++) {
+		if (i == state) {
+			DrawFormatString(0, 0, GetColor(255, 255, 255), "現在マップ%d", i + 1);
+			DrawCircle(map[i].x, map[i].y, 30, GetColor(255, 0, 0), true);
+		}
+		DrawCircle(map[i].x, map[i].y, 20, GetColor(255, 255, 255), true);
 	}
-	for (int j = 0; j < map[player.pos].prev.size(); j++) {
-		DrawFormatString(0 + 80 * j, 60, GetColor(255, 255, 255), "プレブ%d", map[player.pos].prev[j]);
-	}
-	DrawFormatString(0, 0, GetColor(255, 255, 255), "現在マップ%d", player.pos);
-	DrawCircle(player.x, player.y, player.r, GetColor(0, 0, 255));
-	// ソフトの終了 
 
 	return 0;
 }
--- Source/MapSelectScene.h
+++ Source/MapSelectScene.h
@@ -1,6 +1,42 @@
 #include "BaseScene.h"
+#include <iostream>
+#include <vector>
 
 class MapSelectScene : public BaseScene {
+
+public:
+
+	int state = 0;
+
+	const int direction[4][2]{
+		{0,1},
+		{1,0},
+		{0,-1},
+		{-1,0}
+	};
+
+	typedef struct MAP {
+		int x, y, r;
+		int connection[9];
+
+		vector<int>next;//繋がっているマップの情報を入れる(前方)
+		vector<int> prev;//繋がっているマップの情報を入れる(後方)
+	};
+
+	//マウス座標用変数
+	int MouseX = 0, MouseY = 0;
+
+	MAP map[9] = {
+		{100,100,20,{1,1,-1,-1,-1}},
+		{200,400,20,{0,2,-1,-1,-1}},
+		{300,100,20,{1,3,-1,-1,-1}},
+		{400,200,20,{2,4,-1,-1,-1}},
+		{500,600,20,{3,5,-1,-1,-1}},
+		{650,650,20,{4,6,-1,-1,-1}},
+		{750,800,20,{5,7,-1,-1,-1}},
+		{850,200,20,{6,8,-1,-1,-1}},
+		{1200,500,20,{7,8,-1,-1,-1}}
+	};
 
 	MapSelectScene();
 	MapSelectScene(ISceneChanger*);
@@ -9,6 +45,4 @@ class MapSelectScene : public BaseScene {
 	int Update();	//計算処理
 	int Draw();		//描画処理
 	int Close();	//終了処理
-
-
 };

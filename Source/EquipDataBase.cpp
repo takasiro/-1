c3a8c734d9b02815651fcee6a69fdea9b28e672f
CSato2000 <bambeaman334@gmail.com>
2019-11-20 23:22:42 +0900

    [update]装備の確認・装備設定を変更できるようにしました
    動作確認はまだです。ダミーのデータを作成してやるよていです

--- Source/EquipDataBase.cpp
+++ Source/EquipDataBase.cpp
@@ -8,23 +8,37 @@ int EquipDataBase::Initialize() {
 }
 
 int EquipDataBase::GetEqCount(int _charaId) {
-	//for (int i = 0; i < eqData.size(); i++) {
-	//	//eqData[i]
-	//}
-	return 0;
+	int idCount;
+	for (int i = 0; i < eqData.size(); i++) {
+		if(eqData[i]->GetCharaId()==_charaId)idCount++;
+	}
+	return idCount;
 }
 
 
 int EquipDataBase::GetEq(int _charaId, int _eqNum) {
-	return 0;
-}
-
-
-void EquipDataBase::SetEq(int _charaId, int _eqId) {
-	;
+	int tmpNum = 0,eqId = 0;
+	for (int i = 0; i < eqData.size(); i++) {
+		if (eqData[i]->GetCharaId() == _charaId)tmpNum++;
+		if (tmpNum == _eqNum) {
+			eqId = eqData[i]->GetEquipId();
+			break;
+		}
+	}
+	return eqId;
 }
 
 
 void EquipDataBase::DeleteEq(int _charaId, int _eqId) {
-	;
+	int dataId = 0;
+	
+	for (int i = 0; i < eqData.size(); i++) {
+		if (eqData[i]->GetCharaId() == _charaId && eqData[i]->GetEquipId == _eqId) {
+			dataId = eqData[i]->GetId();
+		}
+	}
+
+	eqData.erase(eqData.begin() +(dataId-1));
+
+
 }

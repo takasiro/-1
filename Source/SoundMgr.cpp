ddc5699a1d802eb4efcdbe663d5c8ef0fc20a822
MellMag <yda.tm.2360623@gmail.com>
2019-11-25 15:57:13 +0900

    [update] SoundMgrのSingleton化　読み込み、再生、確認、停止機能の実装

--- Source/SoundMgr.cpp
+++ Source/SoundMgr.cpp
@@ -1,10 +1,11 @@
 #include"SoundMgr.h"
 
 SoundMgr::SoundMgr() {
-
+	mSE["SE01"] = LoadSoundMem("../Sound/se01.wav");
+	mBGM["BGM01"] = LoadSoundMem("../Sound/bgm01.wav");
 }
 SoundMgr:: ~SoundMgr() {
-
+	InitSoundMem();
 }
 int SoundMgr::Initialize() {
 	return 0;
@@ -18,4 +19,26 @@ int SoundMgr::Draw() {
 }
 int SoundMgr::Close() {
 	return 0;
+}
+
+
+int SoundMgr::PlaySE(const char* _SEName) {
+	return PlaySoundMem(mSE[_SEName], DX_PLAYTYPE_BACK, true);
+}
+int SoundMgr::PlayBGM(const char* _BGMName) {
+	return PlaySoundMem(mBGM[_BGMName], DX_PLAYTYPE_LOOP, true);
+}
+
+int SoundMgr::CheckSE(const char* _SEName) {
+	return  CheckSoundMem(mSE[_SEName]);
+}
+int SoundMgr::CheckBGM(const char* _BGMName) {
+	return CheckSoundMem(mSE[_BGMName]);
+}
+
+int SoundMgr::StopSE(const char* _SEName) {
+	return  StopSoundMem(mSE[_SEName]);
+}
+int SoundMgr::StopBGM(const char* _BGMName) {
+	return StopSoundMem(mSE[_BGMName]);
 }

0333b418595050d1f1fd1392e946a77139a3995a
CSato2000 <bambeaman334@gmail.com>
2019-12-05 10:50:26 +0900

    [Update]装備データベースの各種関数の作成
    動作確認済みです。詳しい説明はcppに書いてあります。

--- Source/EquipDataBase.cpp
+++ Source/EquipDataBase.cpp
@@ -1,12 +1,22 @@
 #include"EquipDataBase.h"
 
 int EquipDataBase::Initialize() {
-	for (int i = 0; i = 10; i++) {
+	//デバッグ用に仮データを入れています
+	for (int i = 0; i = 8; i++) {
 		eqData.emplace_back(new EquipData(i,i+1,i+2));
 	}
 	return 0;
 }
 
+/************************************************************************************
+関数名：int EquipDataBase::GetEqCount(int _charaId) 
+引数：int _charaId…キャラクターID
+戻り値:int idCount…装備している武器の数
+機能説明：指定キャラクターの装備している武器の数を取得するときに使用してください。
+		　例)タンク(ID…1)がいくつ武器を装備しているか知りたい場合
+			 int hoge=GetEqCount(1);
+			 →タンクが武器を2つ装備していたらhogeに2が代入されます。
+************************************************************************************/
 int EquipDataBase::GetEqCount(int _charaId) {
 	int idCount;
 	for (int i = 0; i < eqData.size(); i++) {
@@ -15,7 +25,16 @@ int EquipDataBase::GetEqCount(int _charaId) {
 	return idCount;
 }
 
-
+/*****************************************************************************************
+関数名：int EquipDataBase::GetEq(int _charaId, int _eqNum)
+引数：int _charaId…キャラクターID
+	  int _eqNum…何個目の装備か
+戻り値:int eqID…武器ID
+機能説明：指定キャラクターが指定個目に装備している武器のIDを取得するときに使用してください
+		　例)タンク(ID…1)が3つ武器を装備していて、2つ目に装備している武器のIDを知りたい場合
+			 int hoge=GetEq(1,2);
+			 →タンクが武器(ID:99)を装備していたらhogeに99が代入されます。
+*********************************************************************************************/
 int EquipDataBase::GetEq(int _charaId, int _eqNum) {
 	int tmpNum = 0,eqId = 0;
 	for (int i = 0; i < eqData.size(); i++) {
@@ -29,6 +48,30 @@ int EquipDataBase::GetEq(int _charaId, int _eqNum) {
 }
 
 
+/*****************************************************************************************
+関数名：int EquipDataBase::SetEquipData(int _charaId, int _equipId) 
+引数：int _charaId…キャラクターID
+	　int _equipId…武器ID
+戻り値:なし
+機能説明：新しくキャラクターに武器を装備させるときに使用してください。
+		　例)タンク(ID…1)に武器(ID:99)を装備させたい場合
+			 int hoge=SetEquipData(1,99);
+			 →データベース上にタンクが武器ID(99)の武器を装備している情報が追加されます。
+*****************************************************************************************/
+void EquipDataBase::SetEquipData(int _charaId, int _equipId) {
+	eqData.emplace_back(new EquipData(eqData.size() + 1, _charaId, _equipId));
+}
+
+
+/************************************************************************************
+関数名：int EquipDataBase::DeleteEq(int _charaId, int _eqId)
+引数：int _charaId…キャラクターID
+戻り値:なし
+機能説明：指定キャラクターの装備している武器データを消去するときに使用してください。
+		　例)タンク(ID…1)が装備中の武器(ID:99)を外したい場合
+			 int hoge=GetEqCount(1);
+			 →データベース上からタンクが武器(ID:99)装備している情報が消去されます。
+************************************************************************************/
 void EquipDataBase::DeleteEq(int _charaId, int _eqId) {
 	int dataId = 0;
 	
--- Source/EquipDataBase.h
+++ Source/EquipDataBase.h
@@ -14,14 +14,16 @@ using namespace std;
 class EquipDataBase :public BaseTask {
 private:
 
-	class EquipData {
+	class EquipData {  //装備データクラス
 	private:
 		int id;  //ID
 		int charaId;  //キャラクターID
 		int equipId;  //装備品ID
 	public:
+		EquipData() { ; };
 		EquipData(int _id, int _charaId, int _equipId) { id = _id; charaId = _charaId; equipId = _equipId; };
 
+		//各種データの取得関数
 		int GetId() { return id; }
 		int GetCharaId() { return charaId; }
 		int GetEquipId() { return equipId; }
@@ -30,17 +32,19 @@ private:
 		int Close() { return 0; }		//終了処理
 	};
 
-	vector<EquipData*> eqData;
+	vector<EquipData*> eqData;  //装備データ配列
 
 public:
 	EquipDataBase() { Initialize(); };
 	virtual ~EquipDataBase() {};
 
-	void SetEquipData(int _charaId, int _equipId) { eqData.emplace_back(new EquipData(eqData.size()+1, _charaId, _equipId)); };
 
-	int GetEqCount(int _charaId);  //データベースにキャラナンバーiがいくつあるか
-	int GetEq(int _charaId, int _eqNum);  //i番目のキャラのj個目の装備
-	void DeleteEq(int _charaId, int _eqId);  //キャラiのjを装備解除　エラー処理も
+	int GetEqCount(int _charaId);  //特定キャラの装備武器数を取得
+	int GetEq(int _charaId, int _eqNum);  //特定キャラの指定個数目の武器IDを取得
+
+	void SetEquipData(int _charaId, int _equipId);  //装備データの追加
+
+	void DeleteEq(int _charaId, int _eqId);  //特定キャラの装備データを消去
 
 	int Initialize();	//初期化処理
 	int Update() { return 0; }		//計算処理

2126318fda5d4dbc7afe4f625eab27dfb7209042
MellMag <yda.tm.2360623@gmail.com>
2019-12-20 15:08:17 +0900

    [update] winmainで更新処理を追加したバージョン
    マージしないでください

--- Source/WinMain.cpp
+++ Source/WinMain.cpp
@@ -14,21 +14,34 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	SceneMgr sceneMgr;
 
 	EffectMgr::Instance()->Initialize();
-//	EffectMgr::Instance()->PlayEffect("Effect01",0, 0);
+	EffectMgr::Instance()->PlayEffect("Effect01",100, 100);
+
+	int *tmpfile=new int[32];
+	LoadDivGraph("../Resource/Effect/tktk_Sword_5.png", 10,
+		5, 3,
+		192, 192, tmpfile);
+
 
 
 	while (ScreenFlip() == 0 && ProcessMessage() == 0 && ClearDrawScreen() == 0) {
+
+		
 
 		//インターフェース更新
 		Keyboard::Instance()->Update();
 		Mouse::Instance()->Update();
 
+		if (Keyboard::Instance()->Get(KEY_INPUT_SPACE) == 0) {
+			EffectMgr::Instance()->PlayEffect("Effect01", 100, 100);
+		}
 
 		sceneMgr.Update();
 		EffectMgr::Instance()->Update();
 
 		sceneMgr.Draw();
 		EffectMgr::Instance()->Draw();
+
+	//	DrawGraph(0, 0, tmpfile[0], true);
 	}
 
 	DxLib_End(); // DXライブラリ終了処理
